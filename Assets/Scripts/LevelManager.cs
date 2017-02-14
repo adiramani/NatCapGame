04836f95052b171d2837d20b947fa81bc8a7cdb5
Garrett <networkednerd@gmail.com>
2017-02-13 18:28:07 -0800

    Took everything out of the zip and now github will ignore pointless files when pushing and fetching

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    [SerializeField]
+    public GameObject tile1;
+    //public GameObject tile2;
+
+	// Use this for initialization
+	void Start () {
+        createLevel(tile1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if(Input.GetKeyDown(KeyCode.Space))
+        {
+            createLevel(tile2);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            createLevel(tile1);
+
+        }*/
+    }
+
+    private void createLevel(GameObject tile)
+    {
+        
+        float tileSize = tile.GetComponent <SpriteRenderer>().sprite.bounds.size.x;
+        Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
+        for (int y = 0; y < 30; y++)
+        {
+            for(int x = 0; x < 30; x++)
+            {
+                GameObject newTile = Instantiate(tile);
+                newTile.transform.position = new Vector3(worldStart.x + (tileSize * x) - tileSize / 2, worldStart.y - (tileSize * y) - tileSize / 2, 0f);
+                //newTile.GetComponent<TileScript>().setup(new Point((int) newTile.transform.position.x, (int) newTile.transform.position.y));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.black;
+    }
+
+    void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+    }
+}
