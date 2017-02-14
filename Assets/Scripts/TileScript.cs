e7fcc401e48aea71465d09f4e96c1c783604ea39
Garrett <networkednerd@gmail.com>
2017-02-13 19:51:43 -0800

    Edited tile script so that clicking a tile changed its point level

--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -3,25 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TileScript : MonoBehaviour {
-
+    public static int LEVELS = 6;
+    public int[] tileValues;
+    public Color[] levelColors;
     // Use this for initialization
     void Start() {
-
+        tileValues = new int[LEVELS];
+        levelColors = new Color[LEVELS];
+        for (int i = 0; i < LEVELS; i++) {
+            levelColors[i] = new Color(1 - .15F * i, 1 - .15F * i, 1 - .15F * i, 1);
+        }
     }
 
     // Update is called once per frame
     void Update() {
-
     }
 
-    void OnMouseDown()
-    {
-        GetComponent<SpriteRenderer>().color = Color.black;
+    void OnMouseDown() {
+        if (tileValues[0] == LEVELS - 1) {
+            tileValues[0] = 0;
+        }else {
+            tileValues[0]++;
+        }
+        GetComponent<SpriteRenderer>().color = levelColors[tileValues[0]];
     }
 
-    void OnMouseUp()
-    {
-        GetComponent<SpriteRenderer>().color = Color.white;
+    //void OnMouseDown()
+    //{
+    //    GetComponent<SpriteRenderer>().color = Color.black;
+    //}
 
-    }
+    //void OnMouseUp()
+    //{
+    //    GetComponent<SpriteRenderer>().color = Color.white;
+
+    //}
 }
