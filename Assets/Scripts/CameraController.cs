638c85d62632a02b7cd1f7fdf6ebbb332f8b1652
Andrey <ghostmancer@gmail.com>
2017-03-20 14:47:16 -0700

    Pinch-to-zoom

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,21 +4,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-    public float pinchSensitivity = 10.0f;
-    public float zoomSensitivity = 5.0f;
-    public float moveSensitivity = 0.8f;
-    public float zoomRatio = 1.7f / 30f;
+    public float pinchSensitivity = 5.0f;
+    public float mouseZoomSensitivity = 5.0f;
+    public float fingerMoveSensitivity = 0.8f;
+    public float mouseMoveSensitivity = 0.8f;
 
+    public float zoomRatio = 1.7f / 30f;
     public float minZoom = 1.0f;
     public float maxZoom = 20.0f;
 
     float currentZoom;
 
     Camera camera;
-
-    // Pinch to zoom variables
-    public float minPinchSpeed = 5.0F;
-    public float varianceInDistances = 5.0F;
 
     void Start () {
         camera = gameObject.GetComponent<Camera>();
@@ -27,38 +24,43 @@ public class CameraController : MonoBehaviour {
 	
 	void Update () {
         // Scroll wheel for zooming in/out
-        float zoom = Input.GetAxis("Mouse ScrollWheel") * zoomSensitivity;
-        if (zoom != 0 && camera.orthographicSize - zoom > minZoom && camera.orthographicSize - zoom < maxZoom) {
+        float zoom = Input.GetAxis("Mouse ScrollWheel") * mouseZoomSensitivity;
+        if(zoom != 0 && camera.orthographicSize - zoom > minZoom && camera.orthographicSize - zoom < maxZoom) {
             camera.orthographicSize = Mathf.Clamp(camera.orthographicSize - zoom, minZoom, maxZoom);
             currentZoom = camera.orthographicSize;
         }
 
         // Hold right-mouse to move view
-        if (Input.GetMouseButton(1)) {
-            float horizontalMovement = Input.GetAxis("Mouse X") * moveSensitivity * zoomRatio * currentZoom;
-            float verticalMovement = Input.GetAxis("Mouse Y") * moveSensitivity * zoomRatio * currentZoom;
+        if(Input.GetMouseButton(1)) {
+            float horizontalMovement = Input.GetAxis("Mouse X") * mouseMoveSensitivity * zoomRatio * currentZoom;
+            float verticalMovement = Input.GetAxis("Mouse Y") * mouseMoveSensitivity * zoomRatio * currentZoom;
             if(horizontalMovement != 0 && verticalMovement != 0) {
                 camera.transform.Translate(-1 * horizontalMovement, -1 * verticalMovement, 0);
             }
         }
 
+        // Touchscreen camera movement
+        if(Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+            float horizontalMovement = Input.GetTouch(0).deltaPosition.x * fingerMoveSensitivity * zoomRatio * currentZoom;
+            float verticalMovement = Input.GetTouch(0).deltaPosition.y * fingerMoveSensitivity * zoomRatio * currentZoom;
+            Debug.Log("Touch: " + horizontalMovement + ", " + verticalMovement);
+            if (horizontalMovement != 0 && verticalMovement != 0) {
+                camera.transform.Translate(-1 * horizontalMovement, -1 * verticalMovement, 0);
+            }
+        }
+
         // Pinch to zoom, from http://answers.unity3d.com/questions/63909/pinch-zoom-camera.html
-        if(Input.touchCount == 2 && Input.GetTouch(0).phase == TouchPhase.Moved && Input.GetTouch(1).phase == TouchPhase.Moved) {
+        // Debug.Log("TouchCount: " + Input.touchCount + "\nTouchPhase: " + (Input.touchCount > 0 ? Input.GetTouch(0).phase.ToString() : "none"));
+        if (Input.touchCount == 2 && Input.GetTouch(0).phase == TouchPhase.Moved && Input.GetTouch(1).phase == TouchPhase.Moved) {
             Vector2 curDist = Input.GetTouch(0).position - Input.GetTouch(1).position; // current distance between finger touches
             Vector2 prevDist = ((Input.GetTouch(0).position - Input.GetTouch(0).deltaPosition) - (Input.GetTouch(1).position - Input.GetTouch(1).deltaPosition)); // difference in previous locations using delta positions
             float touchDelta = curDist.magnitude - prevDist.magnitude;
-            float speedTouch0 = Input.GetTouch(0).deltaPosition.magnitude / Input.GetTouch(0).deltaTime;
-            float speedTouch1 = Input.GetTouch(1).deltaPosition.magnitude / Input.GetTouch(1).deltaTime;
+            float zoomAmount = touchDelta / Input.GetTouch(0).deltaTime;
 
-            if ((touchDelta + varianceInDistances <= 1) && (speedTouch0 > minPinchSpeed) && (speedTouch1 > minPinchSpeed)) {
-                camera.orthographicSize = Mathf.Clamp(camera.orthographicSize + pinchSensitivity, minZoom, maxZoom);
-                currentZoom = camera.orthographicSize;
-            }
-
-            if ((touchDelta + varianceInDistances > 1) && (speedTouch0 > minPinchSpeed) && (speedTouch1 > minPinchSpeed)) {
-                camera.orthographicSize = Mathf.Clamp(camera.orthographicSize - pinchSensitivity, minZoom, maxZoom);
-                currentZoom = camera.orthographicSize;
-            }
+            // Debug.Log("zA: " + zoomAmount);
+            
+            camera.orthographicSize = Mathf.Clamp(camera.orthographicSize - zoomAmount * pinchSensitivity, minZoom, maxZoom);
+            currentZoom = camera.orthographicSize;
         }
     }
 }
