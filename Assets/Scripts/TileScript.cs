94bf4191897731f2b8230d83023e4e55110c0731
Andrey <ghostmancer@gmail.com>
2017-04-03 16:21:00 -0700

    Color code cleanup

--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -8,7 +8,7 @@ public class TileScript : MonoBehaviour {
     int[] coordinate = new int[2];
     // tile scores and the color cache are stored as dictionaries where the indexes are MapMaps
     public Dictionary<MapModeCanvas.MapMode, int> scores = new Dictionary<MapModeCanvas.MapMode, int>();
-    // color cache exists because it is really slow to recalculate color values when switching mapmode=
+    // color cache exists because it is really slow to recalculate color values when switching mapmode
     public Dictionary<MapModeCanvas.MapMode, Color> colorCache = new Dictionary<MapModeCanvas.MapMode, Color>();
     SpriteRenderer spriteRenderer;
 
@@ -42,27 +42,19 @@ public class TileScript : MonoBehaviour {
     }
 
     public void calculateAllColors() {
-        colorCache[MapModeCanvas.MapMode.PortExpansion] = calculateColor(scores[MapModeCanvas.MapMode.PortExpansion], mapModeCanvas.gridColorSchemes[MapModeCanvas.MapMode.PortExpansion]);
-        colorCache[MapModeCanvas.MapMode.MineralExtraction] = calculateColor(scores[MapModeCanvas.MapMode.MineralExtraction], mapModeCanvas.gridColorSchemes[MapModeCanvas.MapMode.MineralExtraction]);
-        colorCache[MapModeCanvas.MapMode.FoodSecurity] = calculateColor(scores[MapModeCanvas.MapMode.FoodSecurity], mapModeCanvas.gridColorSchemes[MapModeCanvas.MapMode.FoodSecurity]);
-        colorCache[MapModeCanvas.MapMode.TourismPotential] = calculateColor(scores[MapModeCanvas.MapMode.TourismPotential], mapModeCanvas.gridColorSchemes[MapModeCanvas.MapMode.TourismPotential]);
+        colorCache[MapModeCanvas.MapMode.PortExpansion] = calculateColor(scores[MapModeCanvas.MapMode.PortExpansion], MapModeCanvas.MapMode.PortExpansion);
+        colorCache[MapModeCanvas.MapMode.MineralExtraction] = calculateColor(scores[MapModeCanvas.MapMode.MineralExtraction], MapModeCanvas.MapMode.MineralExtraction);
+        colorCache[MapModeCanvas.MapMode.FoodSecurity] = calculateColor(scores[MapModeCanvas.MapMode.FoodSecurity], MapModeCanvas.MapMode.FoodSecurity);
+        colorCache[MapModeCanvas.MapMode.TourismPotential] = calculateColor(scores[MapModeCanvas.MapMode.TourismPotential], MapModeCanvas.MapMode.TourismPotential);
     }
 
-    public Color calculateColor(int score, Color[] colorScheme) {
+    public Color calculateColor(int score, MapModeCanvas.MapMode mapMode) {
         if(score == 0) {
             return new Color(0, 0, 0, 0);
         }
-        Color color = colorScheme[(score / 10) - 1];
+        Color color = mapModeCanvas.gridColorSchemes[mapMode][(score / 10) - 1];
         return new Color(color.r, color.g, color.b, 0.85f);
     }
-
-    /*
-     * 50: 171, 95, 125
-     * 40: 210, 113, 127
-     * 30: 239, 161, 154
-     * 20: 247, 204, 183
-     * 10: 252, 238, 220
-     */
 
     public void changeColor(Color newColor) {
         spriteRenderer.color = newColor;
@@ -74,11 +66,12 @@ public class TileScript : MonoBehaviour {
          * 1) current score updated for current map mode
          * 2) new color is calculated, cached, and set
          */
-        if(levelEditor.editing && !EventSystem.current.IsPointerOverGameObject()) {
+        // !EventSystem.current.IsPointerOverGameObject()
+        if(levelEditor.editing) {
             scores[mapModeCanvas.currentMode] = levelEditor.currentValue;
             Debug.Log(mapModeCanvas.currentMode);
             Debug.Log(mapModeCanvas.gridColorSchemes[mapModeCanvas.currentMode]);
-            Color newColor = calculateColor(scores[mapModeCanvas.currentMode], mapModeCanvas.gridColorSchemes[mapModeCanvas.currentMode]);
+            Color newColor = calculateColor(scores[mapModeCanvas.currentMode], mapModeCanvas.currentMode);
             colorCache[mapModeCanvas.currentMode] = newColor;
             changeColor(newColor);
         }
