2a0683dc8190c7a422bea7f313edd1a88778206d
Andrey <ghostmancer@gmail.com>
2017-04-05 21:16:22 -0700

    Toggling editor UI

--- Assets/Scripts/LevelEditor/LevelEditor.cs
+++ Assets/Scripts/LevelEditor/LevelEditor.cs
@@ -13,7 +13,9 @@ public class LevelEditor : MonoBehaviour {
     public int currentValue = 0;
     
     void Start() {
-
+        if(editing == false) {
+            gameObject.GetComponent<Canvas>().enabled = false;
+        }
     }
 
     public void setBtnValue(int newValue) {
